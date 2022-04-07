da543c07e5e680de08e61ec3ba60414c1d773d6e
01395265 <zhanjinhao@sf-express.com>
2022-04-07 19:24:31 +0800

    suppress BeanPostProcessorChecker#postProcessAfterInitialization info.

--- src/main/java/cn/addenda/businesseasy/propertyrefresh/PropertyRefreshConfiguration.java
+++ src/main/java/cn/addenda/businesseasy/propertyrefresh/PropertyRefreshConfiguration.java
@@ -1,16 +1,23 @@
 package cn.addenda.businesseasy.propertyrefresh;
 
+import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportAware;
+import org.springframework.context.annotation.Role;
 import org.springframework.core.annotation.AnnotationAttributes;
 import org.springframework.core.type.AnnotationMetadata;
 
 /**
+ * 这个配置类被标注为 ROLE_INFRASTRUCTURE 来压制 BeanPostProcessorChecker#postProcessAfterInitialization 的info。
+ * 在此 @Configuration 类中配置的 PropertyRefreshBeanPostProcessor 是晚于 @Configuration 类实例化的。
+ * 所以 PropertyRefreshConfiguration 创建时没法 PropertyRefreshBeanPostProcessor 处理。就会出现一个info。
+ *
  * @Author ISJINHAO
  * @Date 2022/4/2 20:32
  */
 @Configuration
+@Role(BeanDefinition.ROLE_INFRASTRUCTURE)
 public class PropertyRefreshConfiguration implements ImportAware {
 
     protected AnnotationAttributes annotationAttributes;
@@ -18,10 +25,10 @@ public class PropertyRefreshConfiguration implements ImportAware {
     @Override
     public void setImportMetadata(AnnotationMetadata importMetadata) {
         this.annotationAttributes = AnnotationAttributes.fromMap(
-                importMetadata.getAnnotationAttributes(EnablePropertyRefresh.class.getName(), false));
+            importMetadata.getAnnotationAttributes(EnablePropertyRefresh.class.getName(), false));
         if (this.annotationAttributes == null) {
             throw new IllegalArgumentException(
-                    "@EnablePropertyRefresh is not present on importing class " + importMetadata.getClassName());
+                "@EnablePropertyRefresh is not present on importing class " + importMetadata.getClassName());
         }
     }
 
