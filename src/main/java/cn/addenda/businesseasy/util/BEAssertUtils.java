67010d58d1271cfe2aebdf0619fe2fa71eecfa91
isjinhao <isjinhao@163.com>
2022-11-29 19:19:28 +0800

    feature: cn.addenda.businesseasy.util.BEAssertUtils。

---
+++ src/main/java/cn/addenda/businesseasy/util/BEAssertUtils.java
@@ -0,0 +1,27 @@
+package cn.addenda.businesseasy.util;
+
+import org.springframework.util.Assert;
+
+/**
+ * @author addenda
+ * @datetime 2022/11/28 22:49
+ */
+public class BEAssertUtils {
+
+    private BEAssertUtils() {
+
+    }
+
+    public static void notNull(Object condition) {
+        Assert.notNull(condition, "parameter cannot be null. ");
+    }
+
+    public static void notNull(Object condition, String filedName) {
+        Assert.notNull(condition, filedName + " cannot be null. ");
+    }
+
+    public static void notModified(Object condition, String filedName) {
+        Assert.isNull(condition, filedName + " cannot be modified. ");
+    }
+
+}
